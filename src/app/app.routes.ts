864b28ddb0d95705bb01113b0b1fa5e72c0b1b5d
AceGjorgjievski <ace.gjorgjievski6@gmail.com>
2025-03-18 14:48:16 +0100

    feat: add navbar components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { FormComponent } from './views/form/form.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: 'home', component: HomeComponent },
+    { path: 'add',  component: FormComponent},
+    { path: ':id/edit',  component: FormComponent},
+
+    { path: '**', component: NotFoundComponent }
+];
